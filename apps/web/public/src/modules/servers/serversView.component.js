9bd4d5e4bd706808833e1b98cb995ad70f98b4a3
Nadyushka <12nadya11@gmail.com>
2024-08-30 16:22:48 +0300

    first commit

---
+++ apps/web/public/src/modules/servers/serversView.component.js
@@ -0,0 +1,49 @@
+import template from './serversView.template';
+
+const serversView = {
+  template: template(),
+  controller: [
+    'Server',
+    '$stateParams',
+    'NotificationService',
+    'Charts',
+    '$uibModal',
+    function(Server, $stateParams, NotificationService, Charts, $uibModal) {
+      this.server = Server.get({id: $stateParams.id});
+      this.charts = Charts.serverCharts({serverId: $stateParams.id});
+      this.showServerInfo = function() {
+        $uibModal.open({
+          component: 'showPayloadModal',
+          size: 'lg',
+          resolve: {
+            payload: this.server,
+          },
+        }).result.catch(angular.noop);
+      };
+      this.start = function() {
+        if (confirm('Вы хотите запустить сервер?')) {
+          this.server.$start(function() {
+            NotificationService.showSuccess('Сервер запущен');
+          });
+        }
+      };
+
+      this.restart = function() {
+        if (confirm('Вы хотите перезапустить сервер?')) {
+          this.server.$restart(function() {
+            NotificationService.showSuccess('Сервер перезапущен')
+          });
+        }
+      };
+
+      this.stop = function() {
+        if (confirm('Вы хотите остановить сервер?')) {
+          this.server.$stop(function() {
+            NotificationService.showSuccess('Сервер остановлен');
+          });
+        }
+      };
+    }],
+};
+
+export {serversView};
